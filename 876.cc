d99d7c6b7d179d91ca9f667f22d11754c2cb1cbc
manjie <manjie@manjieserver.0.0.31.10.in-addr.arpa>
2023-12-24 10:07:47 +0000

    86 分隔链表

---
+++ 876.cc
@@ -0,0 +1,38 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode dummy;
+        dummy.next = head;
+        ListNode *fast = &dummy, *slow = &dummy;
+        while (fast != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next;
+            if (fast != nullptr)
+            {
+                fast = fast->next;
+            }
+        }
+        return slow;
+    }
+};
