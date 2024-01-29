a76d9774dc136cf525420c4003cbdf2fde918bf6
manjie007 <297867870@qq.com>
2024-01-29 05:52:13 +0000

    104 二叉树的最大深度

---
+++ 104.cc
@@ -0,0 +1,37 @@
+ #include <algorithm>
+
+ using namespace std;
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        
+        int leftMax = maxDepth(root->left);
+        int rightMax = maxDepth(root->right);
+
+        return std::max(leftMax, rightMax) + 1; 
+    }
+};
