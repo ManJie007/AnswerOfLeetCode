5ff0378abbddeab3f2cfb19d2b47474da3d177eb
manjie007 <297867870@qq.com>
2024-01-30 13:09:22 +0000

    226 翻转二叉树

---
+++ 226.cc
@@ -0,0 +1,44 @@
+#include <utility>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    void traverse(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+
+        std::swap(root->left, root->right);
+        traverse(root->left);
+        traverse(root->right);
+    }
+
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        traverse(root);
+        return root;
+    }
+};
