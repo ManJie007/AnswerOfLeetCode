3b63a8c2e6555f21ec6d178bb1339407fdd23dec
manjie007 <297867870@qq.com>
2024-02-04 08:21:44 +0000

    114 二叉树展开为链表

---
+++ 114.cc
@@ -0,0 +1,47 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void flatten(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+
+        flatten(root->left);
+        flatten(root->right);
+
+        TreeNode *tmp_left = root->left;
+        TreeNode *tmp_right = root->right;
+
+        root->left = nullptr;
+        root->right = tmp_left;
+
+        TreeNode *tmp = root;
+        while (tmp->right != nullptr)
+        {
+            tmp = tmp->right;
+        }
+        tmp->right = tmp_right;
+    }
+};
--- 116.cc
+++ 116.cc
@@ -1,17 +1,18 @@
 #include <cstddef>
 
-class Node {
+class Node
+{
 public:
     int val;
-    Node* left;
-    Node* right;
-    Node* next;
+    Node *left;
+    Node *right;
+    Node *next;
 
     Node() : val(0), left(NULL), right(NULL), next(NULL) {}
 
     Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
 
-    Node(int _val, Node* _left, Node* _right, Node* _next)
+    Node(int _val, Node *_left, Node *_right, Node *_next)
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 /*
@@ -32,28 +33,31 @@ public:
 };
 */
 
-class Solution {
+class Solution
+{
 private:
-    void traverse(Node* node1, Node* node2)
+    void traverse(Node *node1, Node *node2)
     {
         if (node1 == nullptr || node2 == nullptr)
         {
             return;
         }
-        
+
         node1->next = node2;
 
         traverse(node1->left, node1->right);
         traverse(node1->right, node2->left);
         traverse(node2->left, node2->right);
     }
+
 public:
-    Node* connect(Node* root) {
-     if (root == nullptr)
-     {
-        return nullptr;
-     }
-      traverse(root->left, root->right);
-      return root;  
+    Node *connect(Node *root)
+    {
+        if (root == nullptr)
+        {
+            return nullptr;
+        }
+        traverse(root->left, root->right);
+        return root;
     }
 };
