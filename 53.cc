b115dfac32822dc0d7d218f052a4255177a9d20d
manjie007 <297867870@qq.com>
2024-05-17 04:47:28 +0000

    53 最大子数组和

--- 53.cc
+++ 53.cc
@@ -4,25 +4,38 @@
 
 using namespace std;
 
+// class Solution {
+// private:
+//   int dp(vector<int> &nums, vector<int> &memo, int j) {
+//
+//     memo[j] = max(memo[j - 1] + nums[j], nums[j]);
+//
+//     return memo[j];
+//   }
+//
+// public:
+//   int maxSubArray(vector<int> &nums) {
+//
+//     vector<int> memo(nums);
+//
+//     int res = memo[0];
+//     for (int i = 1; i < nums.size(); ++i) {
+//       res = max(res, dp(nums, memo, i));
+//     }
+//
+//     return res;
+//   }
+// };
+
 class Solution {
-private:
-  int dp(vector<int> &nums, vector<int> &memo, int j) {
-
-    memo[j] = max(memo[j - 1] + nums[j], nums[j]);
-
-    return memo[j];
-  }
-
 public:
   int maxSubArray(vector<int> &nums) {
+    vector<int> dp(nums.size() + 1, 0);
 
-    vector<int> memo(nums);
-
-    int res = memo[0];
-    for (int i = 1; i < nums.size(); ++i) {
-      res = max(res, dp(nums, memo, i));
+    for (int i = 1; i <= nums.size(); ++i) {
+      dp[i] = max(dp[i - 1] + nums[i - 1], nums[i - 1]);
     }
 
-    return res;
+    return *max_element(dp.begin() + 1, dp.end());
   }
 };
