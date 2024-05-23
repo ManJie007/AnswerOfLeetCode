a003e183f2224ef2e1a83a84b0e6d8f3a13485f3
manjie007 <297867870@qq.com>
2024-05-23 05:08:15 +0000

    41 缺失的第一个正数

---
+++ 41.cc
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < nums.size(); ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
+            }
+        }
+
+        for(int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
