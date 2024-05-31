6bc6e9e2679131e947e543c89a23eac2ea09d2f3
manjie007 <297867870@qq.com>
2024-05-31 07:08:31 +0000

    24 两两交换链表中的节点

---
+++ 24.cc
@@ -0,0 +1,31 @@
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode *newhead = head->next;
+        head->next = swapPairs(head->next->next);
+        newhead->next = head;
+
+        return newhead;
+    }
+};
