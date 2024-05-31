d0efbff201ce6fb581399065a2acdaab7c8b854b
manjie007 <297867870@qq.com>
2024-05-31 05:05:16 +0000

    206 反转链表 加入迭代解法

--- 206.cc
+++ 206.cc
@@ -15,18 +15,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//class Solution {
+//public:
+//    ListNode* reverseList(ListNode* head) {
+//        if (head == nullptr || head->next == nullptr)
+//        {
+//            return head;
+//        }
+//        
+//        ListNode *last = reverseList(head->next);
+//        head->next->next = head;
+//        head->next = nullptr;
+//
+//        return last;
+//    }
+//};
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr)
-        {
-            return head;
-        }
-        
-        ListNode *last = reverseList(head->next);
-        head->next->next = head;
-        head->next = nullptr;
+        ListNode *pre = nullptr, *cur = head;
 
-        return last;
+        while(cur) {
+            ListNode *next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
     }
 };
